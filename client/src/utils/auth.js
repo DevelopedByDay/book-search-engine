0c040f9afdaf28b7518b306a3fbb7499eb4c5d92
BradD <bradleyeday@gmail.com>
2020-10-28 14:46:22 -0500

    utils meant for client side moved

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,25 +1,21 @@
-// use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
+// Use class with refactored graphql
 class AuthService {
-  // get user data
   getProfile() {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
+  // We must check if a user is logged in and if their token is expired 
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token); 
   }
 
-  // check if token is expired
   isTokenExpired(token) {
     try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
+      const codeToken = decode(token);
+      if (codeToken.exp < Date.now() / 1000) {
         return true;
       } else return false;
     } catch (err) {
@@ -28,20 +24,17 @@ class AuthService {
   }
 
   getToken() {
-    // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
 
   login(idToken) {
-    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
 
+  // when logging out we can clear out a user's token as well
   logout() {
-    // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
 }
--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,29 +1,25 @@
 export const getSavedBookIds = () => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-    : [];
+  const savedBookIds = localStorage.getItem('saved_books') ? JSON.parse(localStorage.getItem('saved_books')): [];
 
   return savedBookIds;
 };
 
-export const saveBookIds = (bookIdArr) => {
-  if (bookIdArr.length) {
-    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
-  } else {
+export const saveBookIds = (bookIdList) => {
+  if(bookIdList.length) {
+    localStorage.setItem('saved_books', JSON.stringify(bookIdList));
+  } else{
     localStorage.removeItem('saved_books');
   }
 };
 
 export const removeBookId = (bookId) => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-    : null;
+  const savedBookIds = localStorage.getItem('saved_books') ? JSON.parse(localStorage.getItem('saved_books')): null;
 
-  if (!savedBookIds) {
+  if(!savedBookIds) {
     return false;
   }
 
-  const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+  const updatedSavedBookIds = savedBookIds ?.filter((savedBookId) => savedBookId !== bookId);
   localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
 
   return true;
