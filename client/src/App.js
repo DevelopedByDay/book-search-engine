aa8fd1f721c7b0bb5c36942213628ba6f24c1c1a
BradD <bradleyeday@gmail.com>
2020-10-28 16:43:06 -0500

    app.js in client side updated with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// lesson 21 demonstrates importing apollo before other files within the application 
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// create apollo constant
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: '/graphql',
+})
+
+// must wrap return with Apollo Provider
 function App() {
   return (
+  <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +36,7 @@ function App() {
         </Switch>
       </>
     </Router>
+  </ApolloProvider>
   );
 }
 
